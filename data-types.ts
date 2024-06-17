87b477599ffcb053a071416b170b8ab34b0d118d
Kitan <olakitanosibogun@gmail,com>
2024-06-17 13:29:21 +0100

    Add more datatypes

--- data-types.ts
+++ data-types.ts
@@ -1,9 +1,10 @@
 // STRING
-const a = "I";
-const b = "am";
+const a = "james";
+const b = "is";
 const c = "happy!";
 const abc = a + " " + b + " " + c;
 const abc2 = ` ${a} ${b} ${c}`;
+// const abc3 = a + b + c
 console.log(abc);
 console.log(abc2);
 
